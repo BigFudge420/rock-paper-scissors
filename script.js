3e2f77c9a103f065f3a8b8fb41d22e93e6b82f18
BIgFudge <87513995+BigFudge420@users.noreply.github.com>
2023-06-03 04:51:52 +0530

    feat: Create new function
    Create new function that makes new variable playerSelection case insensitive, i.e it turns the first letter to Upper Case and the rest to Lower Case

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+//  ------------ALGORITHM------------
+
 // create new function getComputerChoice that picks an option from rock, paper and scissors on random.
 // use prompt to get the player's choice 
+// create function that makes the player's choice case insensitve
 // make a function that plays one round and declares winners, with parameters that have the values equal to both the players and computers choice of weapon
 // make a funtion that plays a game, with 5 rounds
+
+//  ------------ALGORITHM------------
 
 let computerSelection
 
@@ -9,13 +14,25 @@ function getComputerChoice(){
     let randomNumber = Math.floor(Math.random()*3);
     switch (randomNumber){
         case 0:
-            return computerSelection = 'rock'
+            return computerSelection = 'Rock'
             break;
         case 1:
-            return computerSelection = 'scissors'
+            return computerSelection = 'Scissors'
             break;
         case 2:
-            return computerSelection = 'paper'
+            return computerSelection = 'Paper'
             break;
     }
 }
+ 
+let playerSelection
+
+function capitalizeOnlyFirstLetter(playerInput = prompt('Choose your weapon!!')){
+    let firstChar = playerInput.charAt(0)
+    let restOfTheString =  playerInput.slice(1)
+    let convertedLetter = firstChar.toUpperCase()
+    let convertedSubstring = restOfTheString.toLowerCase()
+    return playerSelection = convertedLetter + convertedSubstring
+}
+
+
